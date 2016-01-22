61ea6065a4eb4eb5d16cfc3a222cfcd5878d72f2
Alex Mocanu <alexmocanu@Alex.local>
2016-01-22 09:18:37 +0200

    Pushed test code

---
+++ ProgressKitSample/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ProgressKitSample
+//
+//  Created by Alex Mocanu on 22/01/16.
+//  Copyright © 2016 Alex Mocanu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
